ee2901f33248582153c9e61701a3eeaf4aa50556
Rudr-1705 <rudrakshmahajan2006@gmail.com>
2025-12-18 14:36:57 +0530

    Added bloc architecture for authentication

---
+++ mobile/saferoute/lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,121 @@
+import 'package:bloc/bloc.dart';
+import 'package:saferoute/services/auth/auth_provider.dart';
+import 'package:saferoute/services/auth/bloc/auth_event.dart';
+import 'package:saferoute/services/auth/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider)
+    : super(const AuthStateUninitialized(isLoading: true)) {
+    // send email verification
+    on<AuthEventSendEmailVerification>((event, emit) async {
+      await provider.sendEmailVerification();
+      emit(state);
+    });
+
+    //register
+    on<AuthEventRegsiter>((event, emit) async {
+      final email = event.email;
+      final password = event.password;
+      try {
+        await provider.createUser(email: email, password: password);
+        await provider.sendEmailVerification();
+        emit(const AuthStateNeedsverification(isLoading: false));
+      } on Exception catch (e) {
+        emit(AuthStateRegsitering(exception: e, isLoading: false));
+      }
+    });
+
+    // initialise
+    on<AuthEventInitialise>((event, emit) async {
+      await provider.intialize();
+      final user = provider.currentUser;
+      if (user == null) {
+        emit(const AuthStateLoggedOut(isLoading: false, exception: null));
+      } else if (!user.isEmailVerified) {
+        emit(const AuthStateNeedsverification(isLoading: false));
+      } else {
+        emit(AuthStateLoggedIn(user: user, isLoading: false));
+      }
+    });
+
+    // log in
+    on<AuthEventLogin>((event, emit) async {
+      emit(
+        const AuthStateLoggedOut(
+          isLoading: true,
+          exception: null,
+          loadingText: 'Please wait while I log you in..',
+        ),
+      );
+      final email = event.email;
+      final password = event.password;
+      try {
+        final user = await provider.logIn(email: email, password: password);
+        if (!user.isEmailVerified) {
+          emit(const AuthStateLoggedOut(isLoading: false, exception: null));
+          emit(const AuthStateNeedsverification(isLoading: false));
+        } else {
+          emit(const AuthStateLoggedOut(isLoading: false, exception: null));
+          emit(AuthStateLoggedIn(user: user, isLoading: false));
+        }
+      } on Exception catch (e) {
+        emit(AuthStateLoggedOut(isLoading: false, exception: e));
+      }
+    });
+
+    // log out
+    on<AuthEventLogout>((event, emit) async {
+      try {
+        await provider.logOut();
+        emit(const AuthStateLoggedOut(isLoading: false, exception: null));
+      } on Exception catch (e) {
+        emit(AuthStateLoggedOut(isLoading: false, exception: e));
+      }
+    });
+
+    on<AuthEventShouldRegister>((event, emit) {
+      emit(const AuthStateRegsitering(exception: null, isLoading: false));
+    });
+
+    on<AuthEventForgotPassword>((event, emit) async {
+      emit(
+        const AuthStateForgotPassword(
+          exception: null,
+          hasSentEmail: false,
+          isLoading: false,
+        ),
+      );
+      final email = event.email;
+      if (email == null) {
+        return;
+      }
+
+      emit(
+        const AuthStateForgotPassword(
+          exception: null,
+          hasSentEmail: false,
+          isLoading: true,
+        ),
+      );
+
+      bool didSendEmail;
+      Exception? exception;
+      try {
+        await provider.sendPasswordReset(toEmail: event.email!);
+        didSendEmail = true;
+        exception = null;
+      } on Exception catch (e) {
+        didSendEmail = false;
+        exception = e;
+      }
+
+      emit(
+        AuthStateForgotPassword(
+          exception: exception,
+          hasSentEmail: didSendEmail,
+          isLoading: false,
+        ),
+      );
+    });
+  }
+}
---
+++ mobile/saferoute/lib/services/auth/bloc/auth_state.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart' show immutable;
+import 'package:saferoute/services/auth/auth_user.dart';
+
+@immutable
+abstract class AuthState {
+  final bool isLoading;
+  final String? loadingText;
+  const AuthState({
+    required this.isLoading,
+    this.loadingText = 'Please wait a moment',
+  });
+}
+
+class AuthStateUninitialized extends AuthState {
+  const AuthStateUninitialized({required bool isLoading})
+    : super(isLoading: isLoading);
+}
+
+class AuthStateRegsitering extends AuthState {
+  final Exception? exception;
+  const AuthStateRegsitering({required this.exception, required bool isLoading})
+    : super(isLoading: isLoading);
+}
+
+class AuthStateForgotPassword extends AuthState {
+  final Exception? exception;
+  final bool hasSentEmail;
+
+  const AuthStateForgotPassword({
+    required this.exception,
+    required this.hasSentEmail,
+    required bool isLoading,
+  }) : super(isLoading: isLoading);
+}
+
+class AuthStateLoggedIn extends AuthState {
+  final AuthUser user;
+  const AuthStateLoggedIn({required this.user, required bool isLoading})
+    : super(isLoading: isLoading);
+}
+
+class AuthStateNeedsverification extends AuthState {
+  const AuthStateNeedsverification({required bool isLoading})
+    : super(isLoading: isLoading);
+}
+
+class AuthStateLoggedOut extends AuthState with EquatableMixin {
+  final Exception? exception;
+  const AuthStateLoggedOut({
+    required this.exception,
+    required bool isLoading,
+    String? loadingText,
+  }) : super(isLoading: isLoading, loadingText: loadingText);
+
+  @override
+  List<Object?> get props => [exception, isLoading];
+}
